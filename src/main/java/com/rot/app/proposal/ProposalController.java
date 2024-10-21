42dded1627d83bc46b679f4ed325b4c4bfce17af
frankblass <frank.blass@web.de>
2024-10-21 15:20:29 +0200

    Create Proposal added

--- src/main/java/com/rot/app/proposal/ProposalController.java
+++ src/main/java/com/rot/app/proposal/ProposalController.java
@@ -1,9 +1,11 @@
 package com.rot.app.proposal;
 
 
+import com.rot.app.category.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,4 +25,15 @@ public class ProposalController {
         return "proposals";
     }
 
+    @GetMapping("/proposals/new")
+    public String showProposalsForm(Model model) {
+        model.addAttribute("proposal", new Proposal());
+        return "proposal_form";
+    }
+
+    @PostMapping("/proposals/save")
+    public String saveProposal(Proposal proposal) {
+        proposalRepository.save(proposal);
+        return "redirect:/proposals";
+    }
 }
